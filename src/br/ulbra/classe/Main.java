4dce67bb642fbbd56bd485fd6bc7fea025d9dcd1
RedONe06 <andrielejorasdossantos@gmail.com>
2022-05-14 09:27:56 -0300

    :ok_hand: style: english

--- src/br/ulbra/classe/Main.java
+++ src/br/ulbra/classe/Main.java
@@ -5,131 +5,132 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double ma, mp, mh, d, s, a, ep, x1, x2, delta, divisao;
+        double ma, mp, mh, d, s, a, ep, x1, x2, delta, division;
         double n1, n2, n3, n4, p1, p2, y1, y2;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Escolha a sua operação: "
-                + "\n(1)Média"
-                + "\n(2)Média Ponderada"
-                + "\n(3)Média Harmonica"
-                + "\n(4)Distância entre dois pontos"
-                + "\n(5)Soma"
-                + "\n(6)A"
-                + "\n(7)Energia Potencial"
+        System.out.println("Choose your operation: "
+                + "\n(1)Average"
+                + "\n(2)Weighted average"
+                + "\n(3)Harmonic Mean"
+                + "\n(4)Distance between two points"
+                + "\n(5)Sum"
+                + "\n(6)Area of ​​a triangle"
+                + "\n(7)Potencial Energy"
                 + "\n(8)Bháskara");
 
-        int resposta = Integer.parseInt(input.nextLine());
-        switch (resposta) {
+        int answer = Integer.parseInt(input.nextLine());
+        switch (answer) {
             case (1):
-                //media
-                System.out.println("Insira o valor 1:");
+                //average
+                System.out.println("Input your value 1:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor 2:");
+                System.out.println("Input your value 2:");
                 n2 = input.nextDouble();
-                System.out.println("Insira o valor 3:");
+                System.out.println("Input your value 3:");
                 n3 = input.nextDouble();
 
                 ma = (n1 + n2 + n3) / 3;
-                System.out.println("O resultado é: " + ma);
+                System.out.println("The result is: " + ma);
                 break;
             case (2):
-                //média ponderada
-                System.out.println("Insira o valor 1:");
+                //Weighted average
+                System.out.println("Input your value 1:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor 2:");
+                System.out.println("Input your value 2:");
                 n2 = input.nextDouble();
-                System.out.println("Insira o valor 3:");
+                System.out.println("Input your value 3:");
                 n3 = input.nextDouble();
 
                 mp = (n1 * 2 + n2 * 3 + n3 * 4) / 10;
-                System.out.println("O resultado é: " + mp);
+                System.out.println("The result is: " + mp);
                 break;
 
             case (3):
-                //media harmonica
-                System.out.println("Insira o valor 1:");
+                //Harmonic mean
+                System.out.println("Input your value 1:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor 2:");
+                System.out.println("Input your value 2:");
                 n2 = input.nextDouble();
 
                 mh = 2 / (1 / n1 + 1 / n2);
-                System.out.println("O resultado é: " + mh);
+                System.out.println("The result is: " + mh);
                 break;
 
             case (4):
-                //distancia entre dois pontos
-                System.out.println("Insira o valor x1:");
+                //Distance between two points
+                System.out.println("Input x1's value:");
                 p1 = input.nextDouble();
-                System.out.println("Insira o valor x2:");
+                System.out.println("Input x2's value::");
                 p2 = input.nextDouble();
-                System.out.println("Insira o valor y1:");
+                System.out.println("IInput y1's value::");
                 y1 = input.nextDouble();
-                System.out.println("Insira o valor y2:");
+                System.out.println("Input y2's value:");
                 y2 = input.nextDouble();
 
                 d = Math.sqrt(Math.pow(p2 - p1, 2) + Math.pow(y2 - y1, 2));
-                System.out.println("O resultado é: " + d);
+                System.out.println("The result is: " + d);
                 break;
 
             case (5):
-                //soma
-                System.out.println("Insira o valor 1:");
+                //Sum
+                System.out.println("Input your value 1:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor 2:");
+                System.out.println("Input your value 2:");
                 n2 = input.nextDouble();
-                System.out.println("Insira o valor 3:");
+                System.out.println("Input your value 3:");
                 n3 = input.nextDouble();
 
                 s = (n1 + n2 + n3) / 2;
-                System.out.println("O resultado é: " + s);
+                System.out.println("The result is: " + s);
                 break;
 
             case (6):
-                //a
-                System.out.println("Insira o valor S:");
+                //Triangule area
+                System.out.println("Input S's value:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor a:");
+                System.out.println("Input a's value:");
                 n2 = input.nextDouble();
-                System.out.println("Insira o valor b:");
+                System.out.println("Input b's value:");
                 n3 = input.nextDouble();
-                System.out.println("Insira o valor c:");
+                System.out.println("Input c's value:");
                 n4 = input.nextDouble();
 
                 a = Math.sqrt(n1 * (n1 - n2) * (n1 - n3) * (n1 - n4));
-                System.out.println("O resultado é: " + a);
+                System.out.println("The result is: " + a);
                 break;
 
             case (7):
-                System.out.println("Insira o valor k:");
+                System.out.println("IInput k's value:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor x:");
+                System.out.println("Input x's value:");
                 n2 = input.nextDouble();
 
                 ep = (n1 * n2 * n2) / 2;
-                System.out.println("O resultado é: " + ep);
+                System.out.println("The result is: " + ep);
                 break;
 
             case (8):
-                System.out.println("Insira o valor a:");
+                System.out.println("Input a's value:");
                 n1 = input.nextDouble();
-                System.out.println("Insira o valor b:");
+                System.out.println("Input b's value:");
                 n2 = input.nextDouble();
-                System.out.println("Insira o valor c:");
+                System.out.println("Input c's value:");
                 n3 = input.nextDouble();
 
                 delta = Math.sqrt(n1 * n1 - 4 * n2 * n3);
-                divisao = (2 * n2);
+                division = (2 * n2);
 
                 if (delta >= 0) {
-                    if (divisao != 0) {
+                    if (division != 0) {
                         x1 = (-n1 - delta) / (2 * n2);
                         x2 = (-n1 + delta) / (2 * n2);
+                        System.out.println("The result is: " + "\nx1 = " + x1 + "\nx2 = " + x2);
                     } else {
-                        System.out.println("Divisão por zero");
+                        System.out.println("Division per zero");
                     }
                 } else {
-                    System.out.println("Delta negativo");
+                    System.out.println("Negative delta");
                 }
         }
     }
